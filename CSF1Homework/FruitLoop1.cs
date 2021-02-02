a38bf111c27892f99f635aae670c54d1fcb95785
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-02 12:30:36 -0600

    FruitLoop1 class

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            string[] fruitNames = { "apple", "banana", "pear", "orange", "kiwi", "strawberry" };
+
+            for (int i = 0; i < fruitNames.Length; i++)
+            {
+                Console.WriteLine(fruitNames[i]);
+            }
+        }
+    }
+}
