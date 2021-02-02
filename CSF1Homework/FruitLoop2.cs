7ae3b493781303e4733a1e18e6ee9a5cafa12c08
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-02 12:57:31 -0600

    FruitLoop2 class

---
+++ CSF1Homework/FruitLoop2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop2
+    {
+        static void Main(string[] args)
+        {
+            string[] fruitNames = { "apple", "banana", "pear", "orange", "kiwi", "strawberry" };
+
+            for (int i = 0; i < fruitNames.Length; i++)
+            {
+                Console.WriteLine(fruitNames[i]);                
+
+                for (int j = -1; j < i; j++)
+                {
+                    Console.Write(".");
+                }
+
+                Console.Write("\n");
+
+                //could also be written using a switch statement since you know how many times it will run
+                /*switch (i)
+                {
+                    case 0:
+                        Console.WriteLine(".");
+                        break;
+
+                    case 1:
+                        Console.WriteLine("..");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("...");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("....");
+                        break;
+
+                    case 4:
+                        Console.WriteLine(".....");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("......");
+                        break;
+                    
+                    default:
+                        break;
+                }//end switch */
+            }
+        }
+    }
+}
