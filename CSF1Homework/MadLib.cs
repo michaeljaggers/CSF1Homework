c168de87697a4d16cb20e0a3591e5b95fb3a8948
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-02 21:25:01 -0600

    completed mad lib story

--- CSF1Homework/MadLib.cs
+++ CSF1Homework/MadLib.cs
@@ -10,6 +10,7 @@ namespace CSF1Homework
     {
         static void Main(string[] args)
         {
+            Console.Title = "CSF1 Mad Lib";
             Console.WriteLine("Homework: Mad Lib");
             Console.WriteLine("\nPlease enter some information below to do this Mad Lib.  Unless otherwise specified, verbs should be in standard present tense (i.e. 'swim' or 'run').  Adverbs are words modifying verbs (like 'soon', or often ending in 'ly', like 'swiftly').  Adjectives are words that describe nouns (like 'green' or 'descriptive').");
 
@@ -22,7 +23,7 @@ namespace CSF1Homework
             Console.WriteLine("\nAdjective:");
             string adjective = Console.ReadLine();
 
-            Console.WriteLine("\nNoun:");
+            Console.WriteLine("\nNoun (plural}:");
             string noun2 = Console.ReadLine();
 
             Console.WriteLine("\nVerb ending in '-ing':");
@@ -44,7 +45,7 @@ namespace CSF1Homework
             string time = Console.ReadLine();
 
             Console.WriteLine("\nThanks, here's your story:");
-            Console.WriteLine($"One day there was a {noun}");
+            Console.WriteLine($"One day there was a {noun} who loved to {verb}.  Because of this they got into trouble with a local gang, the {adjective} {noun2}. The only reason they got out alive was because {verb2} came naturally to them.  Thinking back, the situation would've been different if they had been able to use their {adjective2} sword.  Now they {adverb} make their way home every night, worried about what could happen if they met again.  I'm sure this time they'd be prepared though and have a battle-cry such as \"{exclamation}\" to try and persuade their opponents from coming any closer to begin with.  Honestly, what else could they do besides move to {city} at {time} when they knew the hoodlums wouldn't be out and about?");
         }
     }
 }
