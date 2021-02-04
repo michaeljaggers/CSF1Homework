eab3f6ff171e00db33d9248fcfcada9d3f97f8af
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-03 18:46:07 -0600

    Finished working console app for Login class

--- CSF1Homework/Login.cs
+++ CSF1Homework/Login.cs
@@ -11,47 +11,72 @@ namespace CSF1Homework
         static void Main(string[] args)
         {
             Console.Title = "CSF1Homework Login";
-
             bool runProgram;
-            int attempts = 3;
-       
+            int userAttempts = 3;
+            int passAttempts = 3;
+            const string CORRECT_NAME = "CHARLES";
+            const string CORRECT_PASSWORD = "PASSWORD";
+
             do
             {
-                //program entry
                 runProgram = true;
                 Console.WriteLine("To proceed, please log in.");
                 Console.WriteLine("Enter your username:\tex. 'charles'\n");
-                const string CORRECT_NAME = "CHARLES";
                 string username = Console.ReadLine().ToUpper();
                 Console.Clear();
 
-
                 if (username == CORRECT_NAME)
                 {
-                    Console.WriteLine("Access Granted.\n");
-                    runProgram = false;
+                    do
+                    {
+                        Console.WriteLine("To continue, please authenticate your account.");
+                        Console.WriteLine("Enter your password:\tex. 'password'\n");
+                        string password = Console.ReadLine().ToUpper();
+                        Console.Clear();
+
+                        if (password == CORRECT_PASSWORD)
+                        {
+                            Console.WriteLine("Access Granted.\n");
+
+                            runProgram = false;
+                        }
+                        else
+                        {
+                            passAttempts--;
+
+                            if (passAttempts > 0)
+                            {
+                                Console.WriteLine("Incorrect password. Access denied.\n" +
+                                                    $"You have {passAttempts} attempt{(passAttempts == 1 ? "" : "s")} remaining.\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Incorrect username. Access denied.\n" +
+                                                  "Your account is now locked, please contact customer service.\n");
+
+                                runProgram = false;
+                            }
+                        } 
+                    } while (runProgram && passAttempts > 0);
                 }
                 else
                 {
-                    attempts--;
+                    userAttempts--;
 
-                    if (attempts > 0)
+                    if (userAttempts > 0)
                     {
-                        Console.WriteLine("Incorrect username: access denied.\n" +
-                                         $"You have {attempts} attempts remaining.\n");
+                        Console.WriteLine("Incorrect username. Access denied.\n" +
+                                         $"You have {userAttempts} attempt{(userAttempts == 1 ? "" : "s")} remaining.\n");
                     } 
                     else
                     {
-                        Console.WriteLine("Incorrect username: access denied.\n" +
-                            "Your account is now locked, please contact customer service.\n");
+                        Console.WriteLine("Incorrect username. Access denied.\n" +
+                                          "Your account is now locked, please contact customer service.\n");
+
                         runProgram = false;
                     }
-                    
-
-
                 }
             } while (runProgram);
-            
-        }
-    }
-}
+        }//end Main
+    }//end class
+}//end namespace
