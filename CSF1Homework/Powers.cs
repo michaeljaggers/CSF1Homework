7d710dfe16d4510ea625494d58883377682bd98c
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-03 21:32:04 -0600

    Added completed Powers class

---
+++ CSF1Homework/Powers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "CSF1 Powers";
+            for (int i = 1; i <= 50; i++)
+            {
+                long result = (long)Math.Pow(2, i);
+                Console.WriteLine($"{result}");
+            }
+        }
+    }
+}
